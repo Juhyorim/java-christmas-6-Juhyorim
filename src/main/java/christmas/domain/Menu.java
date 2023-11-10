f76d62dd6f9743a645102bddcd25d0cacad1ca94
Juhyorim <joohr1234@gmail.com>
2023-11-11 02:22:00 +0900

    feat: 메뉴와 메뉴그룹 생성

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+public enum Menu {
+    MUSHROOM_SOUP(6_000),
+    TAPAS(5_500),
+    CAESAR_SALAD(8_000),
+    T_BONE_STEAK(55_000),
+    BARBECUE_RIBS(54_000),
+    SEAFOOD_PASTA(35_000),
+    CHRISTMAS_PASTA(25_000),
+    CHOCOLATE_CAKE(15_000),
+    ICE_CREAM(5_000),
+    ZERO_COLA(3_000),
+    RED_WINE(60_000),
+    CHAMPAGNE(25_000);
+
+    private int price;
+
+    Menu(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/christmas/domain/MenuGroup.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import java.util.List;
+
+public enum MenuGroup {
+    APPETIZER(List.of(Menu.MUSHROOM_SOUP, Menu.TAPAS, Menu.CAESAR_SALAD)),
+    MAIN(List.of(Menu.T_BONE_STEAK, Menu.BARBECUE_RIBS, Menu.SEAFOOD_PASTA, Menu.CHRISTMAS_PASTA)),
+    DESSERT(List.of(Menu.CHOCOLATE_CAKE, Menu.ICE_CREAM)),
+    DRINK(List.of(Menu.ZERO_COLA, Menu.RED_WINE, Menu.CHAMPAGNE));
+
+    private List<Menu> menus;
+
+    MenuGroup(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public static MenuGroup findMenuGroup(Menu menu) {
+        for (MenuGroup menuGroup: values()) {
+            if (menuGroup.menus.contains(menu)) {
+                return menuGroup;
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 메뉴에 해당하는 그룹이 없습니다");
+    }
+}
