60c9521ae0b3a7f25cb63965be430728bc053d4c
Juhyorim <joohr1234@gmail.com>
2023-11-11 22:39:56 +0900

    test: OrderForm이 메뉴 종류와 개수를 잘 담는가에 대한 테스트 작성

--- src/test/java/christmas/domain/OrderFormTest.java
+++ src/test/java/christmas/domain/OrderFormTest.java
@@ -8,6 +8,22 @@ import org.junit.jupiter.api.Test;
 
 class OrderFormTest {
     @Test
+    @DisplayName("메뉴와 개수를 잘 담는지 테스트")
+    void menuCount() {
+        OrderForm orderForm = new OrderForm(LocalDate.of(2023, 12, 1));
+        orderForm.addMenu(Menu.CHRISTMAS_PASTA, 1);
+        orderForm.addMenu(Menu.CAESAR_SALAD, 1);
+        orderForm.addMenu(Menu.T_BONE_STEAK, 3);
+
+        OrderedMenu menus = orderForm.getMenus();
+
+        assertThat(menus.getKindOfMenu()).contains(Menu.CHRISTMAS_PASTA, Menu.CAESAR_SALAD, Menu.T_BONE_STEAK);
+        assertThat(menus.getCount(Menu.CHRISTMAS_PASTA)).isEqualTo(1);
+        assertThat(menus.getCount(Menu.CAESAR_SALAD)).isEqualTo(1);
+        assertThat(menus.getCount(Menu.T_BONE_STEAK)).isEqualTo(3);
+    }
+
+    @Test
     @DisplayName("총주문금액 테스트")
     void totalPrice() {
         OrderForm orderForm = new OrderForm(LocalDate.of(2023, 12, 1));
