b2d1b7a9acf8ff82c0be9b21b54907e9e240235c
Juhyorim <joohr1234@gmail.com>
2023-11-12 04:07:07 +0900

    feat: 예상 방문날짜가 1일부터 31일 사이가 아닐 때 예외발생 추가

--- src/main/java/christmas/EventDayValidator.java
+++ src/main/java/christmas/EventDayValidator.java
@@ -1,15 +1,26 @@
 package christmas;
 
 public class EventDayValidator {
+    private static final int EVENT_START_DAY = 1;
+    private static final int EVENT_LAST_DAY = 31;
     public static final String NUMERIC_REQUIRED = "날짜는 숫자여야 합니다";
+    public static final String NOT_IN_EVENT_PERIOD = "이벤트 기간이 아닙니다";
 
     public static void validate(String input) {
         validateIsNumeric(input);
+        validateEventPeriod(input);
     }
 
     private static void validateIsNumeric(String input) {
         if (input.matches("[0-9]+") == false) {
             throw new IllegalArgumentException(NUMERIC_REQUIRED);
+        }
+    }
+
+    private static void validateEventPeriod(String input) {
+        Integer inputNumber = Integer.parseInt(input);
+        if (inputNumber < EVENT_START_DAY || inputNumber > EVENT_LAST_DAY) {
+            throw new IllegalArgumentException(NOT_IN_EVENT_PERIOD);
         }
     }
 }
