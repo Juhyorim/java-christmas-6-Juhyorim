675050f127b56f68ef3463e0bc3acdb7e1e67433
Juhyorim <joohr1234@gmail.com>
2023-11-13 01:09:22 +0900

    feat: 메뉴 개수가 1이상의 숫자가 아닐 때 예외발생 기능 구현

--- src/main/java/christmas/util/OrderInputManager.java
+++ src/main/java/christmas/util/OrderInputManager.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 public class OrderInputManager {
+    public static final String INVALiD_MENU = "해당하는 메뉴가 존재하지 않습니다.";
+    public static final String INVALID_MENU_COUNT = "메뉴 개수가 올바르지 않습니다.";
+
     public static Map<Menu, Integer> getValidOrder(String input) {
         List<String> menuCountBundle = menuCountBundleSplit(input);
         return menuAndCountSplit(menuCountBundle);
@@ -20,7 +23,7 @@ public class OrderInputManager {
             String count = splitedMenuCount.get(1);
 
             Menu validMenu = parseMenu(menu);
-            int validCount = Integer.parseInt(count);
+            int validCount = parseMenuCount(count);
             menuAndCount.put(validMenu, validCount);
         }
 
@@ -32,10 +35,25 @@ public class OrderInputManager {
 
         Menu menu = Menu.getMenu(menuName);
         if (menu == null) {
-            throw new IllegalArgumentException("해당하는 메뉴가 존재하지 않습니다.");
+            throw new IllegalArgumentException(INVALiD_MENU);
         }
 
         return menu;
+    }
+
+    private static int parseMenuCount(String count) {
+        count = count.trim();
+
+        if (count.matches("[0-9]+") == false) {
+            throw new IllegalArgumentException(INVALID_MENU_COUNT);
+        }
+
+        int menuCount = Integer.parseInt(count);
+        if (menuCount == 0) {
+            throw new IllegalArgumentException(INVALID_MENU_COUNT);
+        }
+
+        return menuCount;
     }
 
     private static List<String> menuCountBundleSplit(String input) {
