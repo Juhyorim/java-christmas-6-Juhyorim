032c282733af9b074f070948560354962fd31cee
Juhyorim <joohr1234@gmail.com>
2023-11-13 01:11:36 +0900

    feat: 메뉴 개수가 20개를 초과할 때 예외발생 기능 구현

--- src/main/java/christmas/util/OrderInputManager.java
+++ src/main/java/christmas/util/OrderInputManager.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 public class OrderInputManager {
+    private static final int MINIMUN_ORDER_COUNT = 20;
     public static final String INVALiD_MENU = "해당하는 메뉴가 존재하지 않습니다.";
     public static final String INVALID_MENU_COUNT = "메뉴 개수가 올바르지 않습니다.";
+    public static final String UNDER_20_REQUIRED = "메뉴 개수는 20개 이하여야합니다.";
 
     public static Map<Menu, Integer> getValidOrder(String input) {
         List<String> menuCountBundle = menuCountBundleSplit(input);
@@ -16,18 +18,28 @@ public class OrderInputManager {
 
     private static Map<Menu, Integer> menuAndCountSplit(List<String> menuCountBundles) {
         Map<Menu, Integer> menuAndCount = new HashMap<>();
+        int totalCount = 0;
 
         for (String menuCountBundle : menuCountBundles) {
-            List<String> splitedMenuCount = SplitManager.split(menuCountBundle, '-');
-            String menu = splitedMenuCount.get(0);
-            String count = splitedMenuCount.get(1);
-
-            Menu validMenu = parseMenu(menu);
-            int validCount = parseMenuCount(count);
-            menuAndCount.put(validMenu, validCount);
+            totalCount += addMenuAndCount(menuAndCount, menuCountBundle);
+            if (totalCount > MINIMUN_ORDER_COUNT) {
+                throw new IllegalArgumentException(UNDER_20_REQUIRED);
+            }
         }
 
         return menuAndCount;
+    }
+
+    private static int addMenuAndCount(Map<Menu, Integer> menuAndCount, String menuCountBundle) {
+        List<String> splitedMenuCount = SplitManager.split(menuCountBundle, '-');
+        String menu = splitedMenuCount.get(0);
+        String count = splitedMenuCount.get(1);
+
+        Menu validMenu = parseMenu(menu);
+        int validCount = parseMenuCount(count);
+        menuAndCount.put(validMenu, validCount);
+
+        return validCount;
     }
 
     private static Menu parseMenu(String menuName) {
