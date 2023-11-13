736ed04a1efd9e6d8ad1e1a0e7fd6bed65d046f5
Juhyorim <joohr1234@gmail.com>
2023-11-13 09:06:27 +0900

    test: GiftProduct 주문 금액에 따라 증정상품이 지급되는지 확인 테스트 작성

---
+++ src/test/java/christmas/domain/benefit/gift/GiftProductTest.java
@@ -0,0 +1,26 @@
+package christmas.domain.benefit.gift;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.dto.GiftCheck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class GiftProductTest {
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "5000:true", "10000:true", "20000:true", "119999:true",
+                    "120000:false", "120001:false", "130000:false"
+            },
+            delimiter = ':'
+    )
+    @DisplayName("주문 금액에 따라 증정상품 지급 확인")
+    void findPossibleGift(int totalPrice, boolean canGetGift) {
+        GiftCheck giftCheck = new GiftCheck(totalPrice);
+        PossibleGift possibleGift = GiftProduct.getPossibleGift(giftCheck);
+
+        assertThat(possibleGift.giftProducts.isEmpty()).isEqualTo(canGetGift);
+    }
+}
