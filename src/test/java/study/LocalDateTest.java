d8060fbb62a22e8c741843a3854a5422e038097f
Juhyorim <joohr1234@gmail.com>
2023-11-13 09:40:41 +0900

    test: LocalDateTest 공부용 테스트코드 추가

--- src/test/java/study/LocalDateTest.java
+++ src/test/java/study/LocalDateTest.java
@@ -2,13 +2,24 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class LocalDateTest {
+    private final List<DayOfWeek> weekdays = List.of(
+            DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY,
+            DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY
+    );
+
+    private final List<DayOfWeek> weekends = List.of(
+            DayOfWeek.FRIDAY, DayOfWeek.SATURDAY
+    );
+
     @Test
     @DisplayName("연월일 세팅 테스트")
     void yearMonthDaySetting() {
@@ -51,5 +62,20 @@ public class LocalDateTest {
 
         assertThat(date.isAfter(firstDate) || date.isEqual(firstDate)).isTrue();
         assertThat(date.isBefore(lastDate) || date.isEqual(lastDate)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "2023:12:3:true", "2023:12:4:true", "2023:12:7:true", "2023:12:10:true", "2023:12:14:true",
+                    "2023:12:1:false", "2023:12:9:false", "2023:12:15:false", "2023:12:23:false", "2023:12:30:false"
+            },
+            delimiter = ':'
+    )
+    @DisplayName("평일 확인 테스트")
+    void weekdayTest(int year, int month, int dayOfMonth, boolean isWeekday) {
+        LocalDate localDate = LocalDate.of(year, month, dayOfMonth);
+
+        assertThat(weekdays.contains(localDate.getDayOfWeek())).isEqualTo(isWeekday);
     }
 }
