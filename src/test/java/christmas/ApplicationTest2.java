3e70a8d256be8569eb441702a56eb37ff41a4ee4
Juhyorim <joohr1234@gmail.com>
2023-11-15 16:32:41 +0900

    test: 주문날짜 입력관련 통합테스트 작성

--- src/test/java/christmas/ApplicationTest2.java
+++ src/test/java/christmas/ApplicationTest2.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ApplicationTest2 extends NsTest {
     public static final String SIMPLE_ORDER_INPUT = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
@@ -22,6 +25,53 @@ class ApplicationTest2 extends NsTest {
                     "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
             );
         });
+    }
+
+    @Nested
+    @DisplayName("주문날짜 입력 관련")
+    class OrderDate {
+        private static final String INVALID_ORDER_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+        private static final String DAY_FORMAT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+
+        @ParameterizedTest
+        @CsvSource(value = {"1", "2", "3", "7", "13", "17", "23", "31"})
+        @DisplayName("출력 테스트")
+        void printDayOfMonth(String dayOfMonth) {
+            assertSimpleTest(() -> {
+                run(dayOfMonth, SIMPLE_ORDER_INPUT);
+                assertThat(output()).contains(DAY_FORMAT.formatted(dayOfMonth));
+            });
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"1 ", " 2", " 3 ", "   7   "})
+        @DisplayName("공백포함 테스트")
+        void printDayOfMonthWithBlank(String dayOfMonth) {
+            assertSimpleTest(() -> {
+                run(dayOfMonth, SIMPLE_ORDER_INPUT);
+                assertThat(output()).contains(DAY_FORMAT.formatted(dayOfMonth));
+            });
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"0", "32", "33", "34"})
+        @DisplayName("범위 밖의 값 테스트")
+        void notInEventPeriod(String dayOfMonth) {
+            assertSimpleTest(() -> {
+                runException(dayOfMonth, SIMPLE_ORDER_INPUT);
+                assertThat(output()).contains(INVALID_ORDER_DATE);
+            });
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"ㄱ", "ㄴ", "r", "_"})
+        @DisplayName("숫자가 아닐 때 테스트")
+        void notNumeric(String dayOfMonth) {
+            assertSimpleTest(() -> {
+                runException(dayOfMonth, SIMPLE_ORDER_INPUT);
+                assertThat(output()).contains(INVALID_ORDER_DATE);
+            });
+        }
     }
 
     @Override
